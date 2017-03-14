5a263da451a58533db6e086ae28e8d7c8e85d55d
sp2410 <saprakas@syr.edu>
2017-03-14 14:49:00 -0400

    S3 key changed

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   #config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_access_key_id"],                        # required
-    aws_secret_access_key: ENV["AWS_secret_access_key"],                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],                        # required
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],                        # required
     region:                'us-west-2'                  # optional, defaults to 'us-east-1'
 #    host:                  's3.example.com',             # optional, defaults to nil
 #    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
@@ -12,3 +12,21 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'wacbacassetsdonttouch'                          # required
   
 end
+
+# CarrierWave.configure do |config|
+#   #config.fog_provider = 'fog/aws'                        # required
+#   config.fog_credentials = {
+#     provider:              'AWS',                        # required
+#     aws_access_key_id:     aws_key,                        # required
+#     aws_secret_access_key: aws_id,                        # required
+#     region:                'us-west-2'                  # optional, defaults to 'us-east-1'
+# #    host:                  's3.example.com',             # optional, defaults to nil
+# #    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+#   }
+
+#   config.fog_directory  = 'wacbacassetsdonttouch'                          # required
+  
+# end
+
+
+
