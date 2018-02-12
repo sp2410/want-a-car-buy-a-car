3c4991c26fec3958ef1bad8b2799f75d46162af0
saprakas@syr.edu <saprakas@syr.edu>
2018-02-11 18:19:44 -0700

    fixing sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_client do |config|
-	config.redis = {db: 0, url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0'}
+	config.redis = {size: 1, db: 0, url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0'}
 end
 
 Sidekiq.configure_server do |config|
-	config.redis = {db: 0, url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0'}
+	config.redis = {size: 1, db: 0, url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0'}
 end
 
 # # REDIS = Redis.new(:url => )
