d8c883edd4110b3c85d80ed3e13ca70a4e2ece85
saprakas@syr.edu <saprakas@syr.edu>
2018-02-12 02:33:54 -0700

    chnaging final chnages

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,4 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook email linkedin reddit hacker_news)
+  config.allow_sites = %w(twitter facebook email tumblr pinterest)
+  
 end
