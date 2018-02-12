0c15a0e6123134f0bed7ce43f1835837dd3f6757
saprakas@syr.edu <saprakas@syr.edu>
2018-02-11 17:49:56 -0700

    fixing sidekiq

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-worker: bundle exec sidekiq
+web: bundle exec thin start -p $PORT
+worker: bundle exec sidekiq -c 5 -v
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
-Sidekiq.configure_client do |config|
-	config.redis = {db: 1, url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0'}
-end
+# Sidekiq.configure_client do |config|
+# 	config.redis = {db: 1, url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0'}
+# end
 
-Sidekiq.configure_server do |config|
-	config.redis = {db: 1, url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0'}
-end
+# Sidekiq.configure_server do |config|
+# 	config.redis = {db: 1, url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0'}
+# end
 
-# REDIS = Redis.new(:url => )
+# # REDIS = Redis.new(:url => )
