b209343bb9685b925438376efbd8efb1a86817e0
saprakas@syr.edu <saprakas@syr.edu>
2018-02-26 01:11:32 -0700

    add new cars

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,9 +11,9 @@ class CategoriesController < ApplicationController
 
 		@alllistings = Listing.all	
 						
-		@newcars = Listing.approved_new.order("#{sort_column}" + " " + "#{sort_direction}").where(:wholesale => false).limit(20)
-		@usedcars = Listing.approved_used.order("#{sort_column}" + " " + "#{sort_direction}").where(:wholesale => false).limit(20)
-		@wholesalecars = Listing.other_wholesale_listings.order("#{sort_column}" + " " + "#{sort_direction}").limit(20)
+		@newcars = Listing.approved_new.order("#{sort_column}" + " " + "#{sort_direction}").where(:wholesale => false).sample(20)
+		@usedcars = Listing.approved_used.order("#{sort_column}" + " " + "#{sort_direction}").where(:wholesale => false).sample(20)
+		@wholesalecars = Listing.other_wholesale_listings.order("#{sort_column}" + " " + "#{sort_direction}").sample(20)
 
 		@listings = Listing.approved_all
 
