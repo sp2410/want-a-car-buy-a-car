085bf299ca9b03bb9964816106c9c01c67d49fa7
saprakas@syr.edu <saprakas@syr.edu>
2018-03-04 16:56:33 -0700

    fixing bugs

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,4 +1,4 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook email tumblr pinterest)
+  config.allow_sites = %w(twitter facebook email tumblr pinterest google_plus)
   
 end
