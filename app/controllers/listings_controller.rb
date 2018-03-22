492c5e5b86ecbfba3bde5709462f6eca7ebf3d2e
saprakas@syr.edu <saprakas@syr.edu>
2018-03-21 22:08:18 -0700

    adding near for search

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -81,7 +81,14 @@ class ListingsController < ApplicationController
 
 		@sort_column = sort_column
 		@sort_direction = sort_direction		
-		@listings = Listing.search(params)#.order("#{@sort_column}" + " " + "#{@sort_direction}")#.near(@userLocation, 5000000, :order => :distance)
+		@listings = Listing.search(params)
+
+		@radius = params[:radius] ? params[:radius] : 200
+
+		if params[:location]
+			@listings = @listings.near(params[:location].upcase, @radius, order: 'distance DESC')
+		end
+		#.order("#{@sort_column}" + " " + "#{@sort_direction}")#.near(@userLocation, 5000000, :order => :distance)
 		#@listings = Listing.search(params).near(@userLocation, 5000, :order => :distance)
 		# @listings = Listing.search(params)	
 
