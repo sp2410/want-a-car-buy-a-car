1d2e77176cd68c4da2d217c65ec0162740157f48
saprakas@syr.edu <saprakas@syr.edu>
2018-03-21 20:36:23 -0700

    adding near for search

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -997,7 +997,15 @@ class Listing < ActiveRecord::Base
 
 	end
 
+	def self.update_or_create(attributes)
+	  assign_or_new(attributes).save
+	end
 
+	def self.assign_or_new(attributes)
+	  obj = first || new
+	  obj.assign_attributes(attributes)
+	  obj
+	end
 
 
 
