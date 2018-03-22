e89c3174743b3056fef1de4ab95a13f31f3b6c29
saprakas@syr.edu <saprakas@syr.edu>
2018-03-21 23:15:40 -0700

    fixing search radius

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,9 +11,9 @@ class CategoriesController < ApplicationController
 		@repairshopscount = Repairshop.where(:approved => true).count
 		@dealercount = User.get_dealers_count
 						
-		@newcars = Listing.approved_new.order("#{sort_column}" + " " + "#{sort_direction}").where(:wholesale => false).sample(9)
-		@usedcars = Listing.approved_used.order("#{sort_column}" + " " + "#{sort_direction}").where(:wholesale => false).sample(9)
-		@wholesalecars = Listing.other_wholesale_listings.order("#{sort_column}" + " " + "#{sort_direction}").sample(9)
+		@newcars = Listing.approved_new.order("#{sort_column}" + " " + "#{sort_direction}").where(:wholesale => false).sample(6)
+		@usedcars = Listing.approved_used.order("#{sort_column}" + " " + "#{sort_direction}").where(:wholesale => false).sample(6)
+		@wholesalecars = Listing.other_wholesale_listings.order("#{sort_column}" + " " + "#{sort_direction}").sample(6)
  
 		#@carcount = Listing.approved_all.count
 		
