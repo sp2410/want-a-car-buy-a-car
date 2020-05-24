3d41466a4968dfb95f3af77b67b4c52a40d03e87
saurav prakash <sprakash24oct@gmail.com>
2020-05-23 19:50:35 -0700

    removed config

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-config.gem 'redis'
+# config.gem 'redis'
 # Initialize the Rails application.
 Rails.application.initialize!
