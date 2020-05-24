b8ac7da7fbaa13148db79b686a38d03843f42138
saurav prakash <sprakash24oct@gmail.com>
2020-05-23 21:19:38 -0700

    added 0 for db in redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-uri = ENV["REDISCLOUD_URL"] || "redis://localhost:6379/"
-REDIS = Redis.new(:url => uri)
+uri = ENV["REDISCLOUD_URL"] || "redis://localhost:6379"
+REDIS = Redis.new(:url => "#{uri}/0")
