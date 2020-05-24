2e46b85feb5cec926228b5b0383f68c788c10d94
saurav prakash <sprakash24oct@gmail.com>
2020-05-23 21:33:37 -0700

    changed sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_client do |config|
-	config.redis = {size: 1, db: 1, url: ENV["REDISCLOUD_URL"] || 'redis://localhost:6379/0'}
+	config.redis = {size: 7, url: ENV["REDISCLOUD_URL"] || 'redis://localhost:6379/0'}
 end
 
 Sidekiq.configure_server do |config|
-	config.redis = {size: 15, db: 1, url: ENV["REDISCLOUD_URL"] || 'redis://localhost:6379/0'}
+	config.redis = {url: ENV["REDISCLOUD_URL"] || 'redis://localhost:6379/0'}
 	schedule_file = "config/schedule.yml"
   if File.exists?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
